55b0b9fe91d837efbdc0fbf692397d789f2940cd
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-03-14 23:18:35 +1000

    Update Flan permission checks

--- neoforge/src/main/java/cn/leolezury/eternalstarlight/neoforge/compatibility/neoforge/NeoForgeFlanCompatibility.java
+++ neoforge/src/main/java/cn/leolezury/eternalstarlight/neoforge/compatibility/neoforge/NeoForgeFlanCompatibility.java
@@ -6,6 +6,7 @@ import cn.leolezury.eternalstarlight.common.compatibility.ESFlanCompatibility;
 import io.github.flemmli97.flan.api.ClaimHandler;
 import io.github.flemmli97.flan.api.permission.BuiltinPermission;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -13,14 +14,18 @@ import net.minecraft.world.entity.Entity;
 @AutoService(ESFlanCompatibility.class)
 public class NeoForgeFlanCompatibility implements ESFlanCompatibility, NeoForgeModCompatibility {
 
+	public static boolean canInteract(ServerLevel level, Entity entity, BlockPos pos, ResourceLocation permission) {
+		return ClaimHandler.getPermissionStorage(level).getForPermissionCheck(pos).canInteract(entity instanceof ServerPlayer player ? player : null, permission, pos);
+	}
+	
 	@Override
 	public boolean claimRestrictsBlockBreak(ServerLevel level, BlockPos pos, Entity entity) {
-		return !ClaimHandler.canInteract(entity instanceof ServerPlayer player ? player : null, pos, BuiltinPermission.BREAK);
+		return !canInteract(level, entity, pos, BuiltinPermission.BREAK);
 	}
 
 	@Override
 	public boolean claimRestrictsBlockPlace(ServerLevel level, BlockPos pos, Entity entity) {
-		return !ClaimHandler.canInteract(entity instanceof ServerPlayer player ? player : null, pos, BuiltinPermission.PLACE);
+		return !canInteract(level, entity, pos, BuiltinPermission.PLACE);
 	}
 
 }
